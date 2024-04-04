3aa8e0febe3e6e7c9d7b989b4a17a4841a11aa18
tinchx1 <108639112+tinchx1@users.noreply.github.com>
2024-04-04 14:21:27 -0300

    New exercises

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
 let persons = [
   {
     id: 1,
@@ -28,6 +29,15 @@ app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
+app.get("/info", (request, response) => {
+  const totalPersons = persons.length;
+  request.timestamp = new Date();
+  const date = request.timestamp.toLocaleString();
+  response.send(`<p>Phonebook has info for ${totalPersons} people</p>
+  <p>${date}</p>
+  `);
+});
+
 app.get("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
   const person = persons.find((person) => person.id === id);
@@ -39,5 +49,46 @@ app.get("/api/persons/:id", (request, response) => {
   }
 });
 
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const person = request.body;
+  if (!person.name || !person.number) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+  const numberDuplicate = persons.some(
+    (existingPerson) => existingPerson.name === person.name
+  );
+  if (numberDuplicate) {
+    return response.status(400).json({
+      error: "name have to be unique",
+    });
+  }
+  const ids = persons.map((person) => person.id);
+  const maxId = Math.max(...ids);
+  const newNote = {
+    id: maxId + 1,
+    name: person.name,
+    number: person.number,
+  };
+  persons.push(newNote);
+  console.log(newNote);
+  response.json(newNote);
+});
+
+app.use((request, response) => {
+  response.status(404).json({
+    error: "Error 404 Not found page",
+  });
+});
 const PORT = 3001;
-app.listen(PORT, () => {});
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
